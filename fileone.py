4342176c550765eac2790b58c5f4663be1586da2
GeometricalTaco <92734466+GeometricalTaco@users.noreply.github.com>
2022-06-17 10:44:17 +0100

    uwu

--- fileone.py
+++ fileone.py
@@ -1,23 +1,39 @@
 import numpy as np
+import nnfs
+from nnfs.datasets import spiral_data
 
-inputs = [[1.0, 2.0, 3.0, 2.5],
-          [2.0, 5.0, -1.0, 2.0],
-          [-1.5, 2.7, 3.3, -0.8]]
+nnfs.init()
 
-weights = [[0.2, 0.8, -0.5, 1.0], 
-           [0.5, -0.91, 0.26, -0.5], 
-           [-0.26, -0.27, 0.17, 0.87]]
+class Layer_Dense:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
 
-biases = [2.0, 3.0, 0.5]
 
-weights2 = [[0.1, 0.14, 0.5], 
-           [-0.5, 0.12, -0.33], 
-           [-0.44, 0.73, -0.13]]
+class Activation_ReLU:
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
 
-biases2 = [1, 2, -0.5]
+class Activation_Softmax:
+    def forward(self, inputs):
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims = True))
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims = True)
+        self.output = probabilities
 
-layer1_outputs = np.dot(inputs, np.array(weights).T) + biases
 
-layer2_outputs = np.dot(layer1_outputs, np.array(weights2).T) + biases2
+X,y = spiral_data(sample=100, classes=3)
 
-print(layer2_outputs)
+dense1 = Layer_Dense(2,3)
+activation1 = Activation_ReLU()
+
+dense2 = Layer_Dense(3, 3)
+activation2 = Activation_Softmax()
+
+dense1.forward(X)
+activation1.forward(dense1.output)
+
+dense2.forward(activation1.output)
+activation2.forward(dense2.output)
+
